667da52d8d19cb1f64d6f2c7abe0aae0b4b971ee
Rayane <rayanemaciel10@gmail.com>
2022-07-19 22:32:49 -0300

    Fazer conexão http e buscar os top 250 filmes

--- alura-stickers/src/App.java
+++ alura-stickers/src/App.java
@@ -1,5 +1,23 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!!");
+
+        //Fazer conexão http e buscar os top 250 filmes
+        String url = "https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060";
+        URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        //executando o request e pegando o body:
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+        System.out.println(body);
+
+        //pegar só os dados que nos interessam (titulo, poster, classificação)
+        //exibir e manipular dados
     }
 }

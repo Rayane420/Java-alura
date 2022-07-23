65c4873046ac12eeff10b58116702f52657e9a01
Rayane <rayanemaciel10@gmail.com>
2022-07-22 22:29:03 -0300

    extraindo os dados que iremos usar

--- alura-stickers/src/App.java
+++ alura-stickers/src/App.java
@@ -3,21 +3,32 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
-        //Fazer conexão http e buscar os top 250 filmes
+        //1- Fazer conexão http e buscar os top 250 filmes
         String url = "https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060";
         URI endereco = URI.create(url);
         var client = HttpClient.newHttpClient();
         var request = HttpRequest.newBuilder(endereco).GET().build();
-        //executando o request e pegando o body:
+        //1.1- executando o request e pegando o body:
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
         System.out.println(body);
 
-        //pegar só os dados que nos interessam (titulo, poster, classificação)
+        //pegar só os dados que nos interessam(parsear dados) [titulo, poster, classificação...]
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+
         //exibir e manipular dados
+        for (Map<String,String> filme : listaDeFilmes) {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+            System.out.println(filme.get("year"));
+        }
     }
 }

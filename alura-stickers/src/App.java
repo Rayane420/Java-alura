ec1fe32d9a28a44fa8f972d122bd8b414254ea5c
Rayane <rayanemaciel10@gmail.com>
2022-07-23 19:33:00 -0300

    gerando stickers a partir da api do imdb

--- alura-stickers/src/App.java
+++ alura-stickers/src/App.java
@@ -1,4 +1,6 @@
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -17,18 +19,27 @@ public class App {
         //1.1- executando o request e pegando o body:
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
-        System.out.println(body);
 
         //pegar só os dados que nos interessam(parsear dados) [titulo, poster, classificação...]
         var parser = new JsonParser();
         List<Map<String, String>> listaDeFilmes = parser.parse(body);
+        
+       
 
         //exibir e manipular dados
+        var geradora = new GeradoraDeStickers();
         for (Map<String,String> filme : listaDeFilmes) {
-            System.out.println(filme.get("title"));
-            System.out.println(filme.get("image"));
-            System.out.println(filme.get("imDbRating"));
-            System.out.println(filme.get("year"));
+
+            String urlImagem = filme.get("image");
+            String titulo = filme.get("title");
+
+            InputStream inputStream = new URL(urlImagem).openStream();
+            String nomeArquivo = "saida/" + titulo + ".png";
+
+            geradora.criar(inputStream, nomeArquivo);
+
+            System.out.println(titulo);
+            System.out.println();
         }
     }
 }
--- alura-stickers/src/GeradoraDeStickers.java
+++ alura-stickers/src/GeradoraDeStickers.java
@@ -1,16 +1,22 @@
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.InputStream;
 import java.awt.Color;
 import java.awt.Font;
 
 import javax.imageio.ImageIO;
 
 public class GeradoraDeStickers {
-    
-    public void criar() throws Exception{
+
+    public void criar(InputStream inputStream, String nomeArquivo) throws Exception{
         //leitura da imagem
-        BufferedImage imagemOriginal = ImageIO.read(new File("entrada/filmeQualidadeMaior.jpg"));
+
+        
+        //InputStream inputStream = new FileInputStream(new File("entrada/filmeQualidadeMaior.jpg"));
+        //InputStream inputStream = new URL("https://m.media-amazon.com/images/M/MV5BMzE5MDM1NDktY2I0OC00YWI5LTk2NzUtYjczNDczOWQxYjM0XkEyXkFqcGdeQXVyMTQxNzMzNDI@.jpg").openStream();
+       
+        BufferedImage imagemOriginal = ImageIO.read(inputStream);
 
         //cria nova imagem em memória com transparência e com tamanho novo
         int largura = imagemOriginal.getWidth();
@@ -22,21 +28,16 @@ public class GeradoraDeStickers {
         Graphics2D graphics = (Graphics2D) novaImagem.getGraphics();
         graphics.drawImage(imagemOriginal, 0, 0, null);
 
-        //Editar Fonte padrão
-        var fonte = new Font(Font.SANS_SERIF, Font.BOLD, 80);
+        //Configurações da Fonte
+        var fonte = new Font(Font.SANS_SERIF, Font.BOLD, 100);
         graphics.setColor( new Color( 57, 255, 20 ) );
         graphics.setFont(fonte);
 
         //add uma frase para a nova imagem
-        graphics.drawString("Top 250 IMDB", 240, novaAltura - 80);
+        graphics.drawString("Top 250 IMDB", 160, novaAltura - 80);
 
-        //escrever a nova imagem em um arquivos
-        ImageIO.write(novaImagem, "png", new File("saida/figurinha.png"));
-    }
-    public static void main(String[] args) throws Exception {
-        //chamando método para gerar o sticker
-        var geradora = new GeradoraDeStickers();
-        geradora.criar();
+        //escrever a nova imagem em um arquivo
+        ImageIO.write(novaImagem, "png", new File(nomeArquivo));
     }
 
 }
